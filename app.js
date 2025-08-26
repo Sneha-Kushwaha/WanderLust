9ecdcb2381ab02894e13e2c3b6ca499d30895d76
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-08-26 10:52:02 +0530

    Created update Route

--- app.js
+++ app.js
@@ -43,6 +43,18 @@ app.get("/listings/:id", async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing});
+});
+
+// Create Route
+app.post("/listings", async (req,res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+// Edit Route
+app.get("/listings/:id/edit", (req,res) => {
+    
 })
 
 // app.get("/testListing", async (req, res) => {
