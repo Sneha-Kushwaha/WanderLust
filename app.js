a6871d9fc6a350d63b80ee8ad2ccd5bbdfb4ad47
Sneha Kushwaha <snehakushwaha7394@gmail.com>
2025-09-22 11:24:17 +0530

    implemented flash messages

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
@@ -27,25 +28,38 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // sessions
 const sessionOptions = {
     secret: "mysupersecretcode",
     resave: false,
     saveUnintialized: true,
-}
-
-app.use(session(sessionOptions));
-
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+};
 
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 });
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
 
 app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
@@ -57,7 +71,7 @@ app.use((req, res, next) => {
 
 
 app.use((err, req, res, next) => {
-    let {statusCode = 500, message = "Something went wrong"} = err;
+    let { statusCode = 500, message = "Something went wrong" } = err;
     res.render("error.ejs", { err });
     // res.status(statusCode).send(message);
 });
--- routes/listing.js
+++ routes/listing.js
@@ -31,6 +31,10 @@ router.get("/new",  wrapAsync(async(req,res) => {
 router.get("/:id",  wrapAsync (async (req, res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+        req.flash("error", "Listing you requested fro does not exits!");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs", {listing});
 }));
 
@@ -40,6 +44,7 @@ router.post("/",
      wrapAsync(async (req,res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 })
 );
@@ -48,6 +53,10 @@ router.post("/",
 router.get("/:id/edit",  wrapAsync (async(req,res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing you requested fro does not exits!");
+        res.redirect("/listings");
+    }
     res.render("listings/edit.ejs", { listing });
 }));
 
@@ -57,6 +66,7 @@ router.put("/:id",
     wrapAsync (async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 }));
 
@@ -65,6 +75,7 @@ router.delete("/:id",  wrapAsync (async (req,res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
+    req.flash("success", "Listing Deleted");
     res.redirect("/listings");
 })
 );
